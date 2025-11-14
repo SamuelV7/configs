b286a8f17c05277f4680aae69bca95b18687d961
Samuel Varghese <15982645+SamuelV7@users.noreply.github.com>
2025-11-13 20:15:37 -0500

    added nixos

---
+++ nixos/flake.nix
@@ -0,0 +1,67 @@
+{
+  description = "A simple NixOS flake";
+
+  inputs = {
+    # NixOS official package source
+    # nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    # zen-browser.url = "github:0xc000022070/zen-browser-flake";
+  };
+
+  # outputs = { self, nixpkgs, zen-browser, ... }: let 
+  outputs = { self, nixpkgs, ... }: let 
+    system = "x86_64-linux";
+    # pkgs = import nixpkgs {
+    #   system = system;
+    #   overlays = [ zen-browser.overlays.default ]; # Ensure this is correct
+    # };
+  in {
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+      # Make this configurable for different architectures
+      inherit system;
+      # specialArgs = { inherit zen-browser; };
+
+
+      modules = [
+        ./configuration.nix
+
+        ({ pkgs, ... }: {
+          environment.systemPackages = with pkgs; [
+            fish
+            htop
+            unzip
+            git
+            btop
+            jujutsu
+            gh
+            python314
+	    bitwarden-desktop
+            ghostty
+            gcc
+            distrobox
+            docker
+            pavucontrol
+            fastfetch
+	    # zen-browser.packages."${system}".default
+          ];
+
+          fonts = {
+            enableDefaultFonts = true;
+            packages = with pkgs; [
+              noto-fonts
+              noto-fonts-cjk-sans
+              noto-fonts-color-emoji
+              liberation_ttf
+              fira-code
+              fira-code-symbols
+              mplus-outline-fonts.githubRelease
+              dina-font
+              proggyfonts
+              jetbrains-mono
+            ];
+          };
+        })
+      ];
+    };
+  };
+}
